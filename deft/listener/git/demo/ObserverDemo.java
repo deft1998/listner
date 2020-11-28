cec14b5c0ee754ca96e7fcc13d44dfa587937cb4
deft1998 <46297026+deft1998@users.noreply.github.com>
2020-11-28 18:03:14 +0800

    Update ObserverDemo.java

--- deft/listener/git/demo/ObserverDemo.java
+++ deft/listener/git/demo/ObserverDemo.java
@@ -7,6 +7,6 @@ public class ObserverDemo implements MyObserver {
     @Override
     public void update(MyObservable o, Object arg) {
         System.out.println(
-                "数据发生改变，测试成功！哒哒哒哒哒dd哒1231");
+                "数据发生改变，测试成功！测试3.0被修改了啦");
     }
 }
